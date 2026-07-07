e702d5129b21ee2764efd0ca1561655c6c862c98
Abadie patrick <abadie.patrick27@gmail.com>
2026-07-07 16:10:13 +0200

    Dev header + argu (#2)
    * !
    
    * add argumuents in bottom hero + header banner rework

--- src/atoms/Button.jsx
+++ src/atoms/Button.jsx
@@ -1,6 +1,9 @@
 export default function Button({ text }) {
   return (
-    <button className="flex items-center gap-2 ml-4 rounded-xl border border-bordercolor bg-background px-4 py-4 font-[Inter] text-white cursor-pointer hover:bg-primary hover:text-black">
+    <button
+      className="flex items-center gap-2 ml-4 rounded-xl border border-bordercolor bg-background px-4 py-4 font-[Inter] text-white cursor-pointer hover:bg-primary hover:text-black hover:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]
+            shadow-primary"
+    >
       <p>{text}</p>
     </button>
   );
--- src/atoms/ButtonArrow.jsx
+++ src/atoms/ButtonArrow.jsx
@@ -2,7 +2,10 @@ import { ArrowRight } from "lucide-react";
 
 export default function Button({ text }) {
   return (
-    <button className="flex items-center gap-2  rounded-xl border border-bordercolor bg-background px-4 py-4 font-[Inter] text-white cursor-pointer hover:bg-primary hover:text-black">
+    <button
+      className="flex items-center gap-2  rounded-xl border border-bordercolor bg-background px-4 py-4 font-[Inter] text-white cursor-pointer hover:bg-primary hover:text-black hover:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]
+            shadow-primary"
+    >
       <p>{text}</p>
       <ArrowRight size={16} />
     </button>
